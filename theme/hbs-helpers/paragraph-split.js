f7ec66689f126210db7f7be90e3897a894efd734
4lch4 <git@4lch4.email>
2024-04-13 15:52:46 -0500

    chore: added & executed prettier

--- theme/hbs-helpers/paragraph-split.js
+++ theme/hbs-helpers/paragraph-split.js
@@ -1,16 +1,16 @@
-const { SafeString } = require('handlebars');
-const markdown = require('markdown-it')();
+const { SafeString } = require('handlebars')
+const markdown = require('markdown-it')()
 
-const paragraphSplit = (text) => {
-  const expr = /\r\n|\r|\n/g;
+const paragraphSplit = text => {
+  const expr = /\r\n|\r|\n/g
 
   if (text != null) {
-    text = markdown.render(text);
+    text = markdown.render(text)
   }
 
-  const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr);
-  const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
-  return new SafeString(output);
-};
+  const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr)
+  const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '')
+  return new SafeString(output)
+}
 
-module.exports = { paragraphSplit };
+module.exports = { paragraphSplit }
