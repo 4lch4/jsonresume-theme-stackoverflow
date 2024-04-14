85bf222a5f3914a757a7c376f2adf51ce86c4a80
4lch4 <git@4lch4.email>
2024-04-14 01:06:13 -0500

    chore: cleaned up test file logic/code

--- test/index.ts
+++ test/index.ts
@@ -1,40 +1,26 @@
 import { watch } from 'fs/promises'
 import path from 'path'
-import { render } from '../src'
 import pc from 'picocolors'
+import { render } from '../src'
 
-const ResumePath = process.argv[2]
+const ResumeInPath = process.argv[2]
   ? path.resolve(process.argv[2])
   : '/home/alcha/Development/Projects/Resumes/Resume/Full-Resume.json'
 
-async function test() {
-  console.log(pc.yellow('Rendering test resume...'))
-
-  const resumeContent = await Bun.file(ResumePath).json()
-
-  console.log(pc.yellow('Loaded test resume content...'))
-
-  const rendered = await render(resumeContent)
-
-  console.log(pc.yellow('Rendered test resume content...'))
-
-  const outPath = path.join(__dirname, 'Latest-Full-Resume.html')
-
-  await Bun.write(outPath, rendered)
-
-  console.log(pc.yellow('Wrote rendered test resume content to:'), outPath)
-}
+const ResumeOutPath = path.join(__dirname, '..', 'Latest-Full-Resume.html')
 
 console.log(pc.yellow('Watching Full-Resume.json for changes...'))
 
-const watcher = watch(ResumePath)
+const watcher = watch(ResumeInPath)
 
 for await (const { eventType } of watcher) {
   if (eventType === 'change') {
-    console.log(pc.green('Detected change in Full-Resume.json'))
+    const resumeContent = await Bun.file(ResumeInPath).json()
 
-    await test()
+    const rendered = await render(resumeContent)
 
-    console.log(pc.green('Re-rendered test resume content...'))
+    await Bun.write(ResumeOutPath, rendered)
+
+    console.log(pc.green(`${path.basename(ResumeInPath)} has changed, re-render complete!`))
   }
 }
